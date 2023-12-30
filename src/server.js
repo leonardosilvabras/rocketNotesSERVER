1c69472669ed1df16ad5ba2cf924e4c1106c0c4f
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2023-12-30 11:19:13 -0300

    aplicando o post json

--- src/server.js
+++ src/server.js
@@ -4,22 +4,12 @@ const express = require("express"),
   app = express(),
   PORT = 3333;
 
-app.get("/message/:id/:user", (request, response) => {
-  const { id, user } = request.params;
+app.use(express.json());
 
-  response.send(`
-    ID: ${id},
-    User: ${user}.
-  `);
+app.post("/users", (req, res) => {
+  const { name, email, password } = req.body;
+
+  res.json({ name, email, password });
 });
-
-app.get('/users', (request, response) => {
-  const { page, limit } = request.query;
-
-  response.send(`
-    Page: ${page},
-    Limit: ${limit}.
-  `);
-})
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
