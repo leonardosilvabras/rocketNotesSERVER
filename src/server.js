9973b66962bfba885c7671a3b9462738450645fd
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2023-12-30 10:59:46 -0300

    get Users and nodemon

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+const req = require("express/lib/request");
+
 const express = require("express"),
   app = express(),
   PORT = 3333;
@@ -6,9 +8,18 @@ app.get("/message/:id/:user", (request, response) => {
   const { id, user } = request.params;
 
   response.send(`
-    ID Message: ${id},
-    for User: ${user}.
+    ID: ${id},
+    User: ${user}.
   `);
 });
+
+app.get('/users', (request, response) => {
+  const { page, limit } = request.query;
+
+  response.send(`
+    Page: ${page},
+    Limit: ${limit}.
+  `);
+})
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
