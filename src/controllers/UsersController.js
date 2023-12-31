b6508a92b9ee1730da641bda0fd12705dd9d00b3
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2023-12-31 15:23:14 -0300

    correcao erro update

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -24,34 +24,40 @@ class UsersController {
     return res.status(201).json();
   }
 
-  async update(req, res) {
-    const { name, email } = req.body,
-      { id } = req.params,
-      database = await sqliteConnection();
+  async update(request, response) {
+    const { name, email } = request.body;
+    const { id } = request.params;
 
+    const database = await sqliteConnection();
     const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
-    if (!user) throw new AppError("Usuario nao encontrado");
+
+    if (!user) {
+      throw new AppError("Usuário não encontrado");
+    }
 
     const userWithUpdatedEmail = await database.get(
       "SELECT * FROM users WHERE email = (?)",
       [email]
     );
-    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id)
-      throw new AppError("Este email ja esta em uso");
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está em uso.");
+    }
 
     user.name = name;
     user.email = email;
 
-    await database.run(`
-      UPDATE users SET
-      name = ?, 
-      email = ?, 
-      updated_at = ?, 
-      id = ?`,
+    await database.run(
+      `
+     UPDATE users SET
+     name = ?,
+     email = ?,
+     updated_at = ?
+     WHERE id = ?`,
       [user.name, user.email, new Date(), id]
     );
 
-    return res.status(200).json();
+    return response.json();
   }
 }
 
