596a9b9395840a9644b280066c35744e41d13b90
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2023-12-31 15:40:06 -0300

    atulizacao de senha com verificacao de senha anterior

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,6 @@
 const AppError = require("../utils/AppError.js"),
   sqliteConnection = require("../database/sqlite"),
-  { hash } = require("bcryptjs");
+  { hash, compare } = require("bcryptjs");
 
 class UsersController {
   async create(req, res) {
@@ -25,7 +25,7 @@ class UsersController {
   }
 
   async update(request, response) {
-    const { name, email } = request.body;
+    const { name, email, password, old_password } = request.body;
     const { id } = request.params;
 
     const database = await sqliteConnection();
@@ -47,14 +47,28 @@ class UsersController {
     user.name = name;
     user.email = email;
 
+    if (password && !old_password)
+      throw new AppError(
+        "Voce precisa informar a senha antiga para redefinir a nova senha"
+      );
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if (!checkOldPassword) throw new AppError("A senha antiga nao confere");
+
+      user.password = await hash(password, 8);
+    }
+
     await database.run(
       `
      UPDATE users SET
      name = ?,
      email = ?,
+     password = ?,
      updated_at = ?
      WHERE id = ?`,
-      [user.name, user.email, new Date(), id]
+      [user.name, user.email, user.password, new Date(), id]
     );
 
     return response.json();
